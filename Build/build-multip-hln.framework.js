0a431940bb6df09682be78c77bf4c717148a4e07
mcbarrygonzalesMsys <mbcgonzales@multyisyscorp.com>
2023-06-23 16:16:38 +0800

    Update

--- Build/build-multip-hln.framework.js
+++ Build/build-multip-hln.framework.js
@@ -1984,13 +1984,13 @@ var tempI64;
 // === Body ===
 
 var ASM_CONSTS = {
-  4410368: function() {Module['emscripten_get_now_backup'] = performance.now;},  
- 4410423: function($0) {performance.now = function() { return $0; };},  
- 4410471: function($0) {performance.now = function() { return $0; };},  
- 4410519: function() {performance.now = Module['emscripten_get_now_backup'];},  
- 4410574: function() {return Module.webglContextAttributes.premultipliedAlpha;},  
- 4410635: function() {return Module.webglContextAttributes.preserveDrawingBuffer;},  
- 4410699: function() {return Module.webglContextAttributes.powerPreference;}
+  4410592: function() {Module['emscripten_get_now_backup'] = performance.now;},  
+ 4410647: function($0) {performance.now = function() { return $0; };},  
+ 4410695: function($0) {performance.now = function() { return $0; };},  
+ 4410743: function() {performance.now = Module['emscripten_get_now_backup'];},  
+ 4410798: function() {return Module.webglContextAttributes.premultipliedAlpha;},  
+ 4410859: function() {return Module.webglContextAttributes.preserveDrawingBuffer;},  
+ 4410923: function() {return Module.webglContextAttributes.powerPreference;}
 };
 
 
